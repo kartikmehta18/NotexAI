fe7e772a69b6c3126c598b4ee6191693024e1847
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-16 03:33:47 +0530

    setupbackend

---
+++ app/provider.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+function Provider({ children }) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+  return (
+    <div>
+     <ConvexProvider client={convex}>{children}</ConvexProvider>
+    </div>
+  );
+}
+
+export default Provider;
